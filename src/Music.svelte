d4c3aa23943e087c8574d6cbf5f212d57640d101
Irshad P I <irshadpi77@gmail.com>
2020-06-16 23:28:54 +0530

    Refactoring Music.svelte to use standard notes, but preseve existing songs. This will unblock #12

--- src/Music.svelte
+++ src/Music.svelte
@@ -3,23 +3,11 @@
 
 	let synth;
 
-	let noteValues = [
-		246.94, 277.18, 311.13, 369.99,
-		392.00, 415.30, 466.16, 554.37,
-		587.33, 622.25, 659.26, 739.99,
-		830.61, 932.33, 987.77, 1046.50,
-		1108.73, 1244.51, 1479.98, 1661.22,
-		1864.66, 2093.00, 2217.46, 2637.02,
-		2959.96, 3322.44, 3520.00, 3729.31
+	let notes = [
+		'B4', 'C#4', 'F#4', 'G#4',
+		'C#5', 'D#5', 'E5', 'G#5',
+		'B5', 'C#6', 'F#6', 'G#6'
 	];
-
-	const rangeMap = (a, b) => s => {
-		const [a1, a2] = a;
-		const [b1, b2] = b;
-		return (((((b2 - b1) * (s - a1)) / (a2 - a1)) * 10) + (10 * b1)) / 10;
-	};
-
-	const mapping = rangeMap([0, 4095], [200, 3000])
 
 	export const initAudio = async () => {
 		synth = new Tone.PolySynth(4, Tone.Synth, {
@@ -40,11 +28,11 @@
 			console.error("Please initialize audio before playing a row");
 			return;
 		}
-		const rowValue = parseInt(row.map(x => x ? '1' : '0' ).reverse().join(''), 2);
+
 		let notesToPlay = []
 		for (var i = row.length - 1; i >= 0; i--) {
 			if(row[i]) {
-				notesToPlay.push(noteValues[Math.floor(i * 1.8)]);
+				notesToPlay.push(notes[i]);
 			}
 		}
 		synth.triggerAttackRelease(notesToPlay, "16n");
